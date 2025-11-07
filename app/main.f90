174b9e3dab04458a6c64c83c8dca57c83122854f
Jean-Christophe <loiseau.jc@gmail.com>
2025-11-07 15:22:03 +0100

    Standard fpm folder architecture.

---
+++ app/main.f90
@@ -0,0 +1,5 @@
+program main
+  implicit none
+
+  print *, "hello from project Navier-Stokes-scientific-computing-project"
+end program main
---
+++ src/Navier-Stokes-scientific-computing-project.f90
@@ -0,0 +1,10 @@
+module Navier_Stokes_scientific_computing_project
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, Navier-Stokes-scientific-computing-project!"
+  end subroutine say_hello
+end module Navier_Stokes_scientific_computing_project
