0696b020b7727686e55a73b667f2fbfc1688ec1f
Jean-Christophe <loiseau.jc@gmail.com>
2025-12-01 09:22:11 +0100

    Minor changes

--- src/linalg.f90
+++ src/linalg.f90
@@ -1,5 +1,124 @@
 module linalg
+   use constants
    implicit none(type, external)
    private
+
+   public :: dense_matvec
+   public :: dense_solve
+   public :: tridiagonal, dense
+   public :: tridiag_matvec
+   public :: tridiag_solve
+
+   !----------------------------------------
+   !-----     DENSE LINEAR ALGEBRA     -----
+   !----------------------------------------
+
+   interface
+      !! Function to perform the matrix-vector product y = Ax where
+      !! A is an arbitrary m x n dense matrix.
+      module function dense_matvec(A, x) result(y)
+         real(wp), intent(in)  :: A(:, :)
+         !! Arbitrary m x n dense matrix.
+         real(wp), intent(in)  :: x(:)
+         !! n-vector.
+         real(wp), allocatable :: y(:)
+         !! m-vector.
+      end function
+   end interface
+
+   interface
+      !! Function to solve the linear system Ax = b using Gaussian
+      !! elimination.
+      module function dense_solve(A, b) result(x)
+         real(wp), intent(in)  :: A(:, :)
+         !! n x n dense matrix describing the linear system.
+         real(wp), intent(in)  :: b(:)
+         !! Right-hand side n-vector.
+         real(wp), allocatable :: x(:)
+         !! Solution n-vector.
+      end function
+   end interface
+
+   !----------------------------------------------
+   !-----     TRIDIAGONAL MATRIX ALGEBRA     -----
+   !----------------------------------------------
+
+   ! Derived-type (i.e. object) to represent a tridiagonal matrix
+   ! with constant coefficients.
+   type :: tridiagonal
+      real(wp) :: a, b, c
+      !! Lower-diagonal, diagonal and upper-diagonal entries of
+      !! the tridiagonal matrix.
+      integer  :: n
+      !! Dimension of the matrix.
+   end type
+
+   interface
+      module function dense(A) result(B)
+         type(tridiagonal), intent(in) :: A
+         real(wp), allocatable :: B(:, :)
+      end function
+   end interface
+
+   interface
+      module function tridiag_matvec(A, x) result(y)
+         type(tridiagonal), intent(in) :: A
+         real(wp), intent(in)  :: x(:)
+         real(wp), allocatable :: y(:)
+      end function
+   end interface
+
+   interface
+      module function tridiag_solve(A, b) result(x)
+         type(tridiagonal), intent(in) :: A
+         real(wp), intent(in)  :: b(:)
+         real(wp), allocatable :: x(:)
+      end function
+   end interface
+
 contains
+
+   !----------------------------------------
+   !-----     DENSE LINEAR ALGEBRA     -----
+   !----------------------------------------
+
+   module procedure dense_matvec
+   integer :: n
+   ! Vector dimension + memory allocation for the result.
+   n = size(A, 1); allocate (y(n), source=0.0_wp)
+   end procedure
+
+   module procedure dense_solve
+   integer :: n
+   ! Vector dimension + memory allocation for the result.
+   n = size(b); allocate (x(n), source=0.0_wp)
+   end procedure
+
+   !----------------------------------------------
+   !-----     TRIDIAGONAL MATRIX ALGEBRA     -----
+   !----------------------------------------------
+
+   module procedure dense
+   integer :: n, i
+   n = A%n; allocate (B(n, n), source=0.0_wp)
+   B(1, 1) = A%b; B(1, 2) = A%c
+   do i = 2, n - 1
+      B(i, i - 1) = A%a
+      B(i, i) = A%b
+      B(i, i + 1) = A%c
+   end do
+   B(n, n - 1) = A%a; B(n, n) = A%b
+   end procedure
+
+   module procedure tridiag_matvec
+   integer :: n
+   ! Vector dimension + memory allocation for the result.
+   n = A%n; allocate (y(n), source=0.0_wp)
+   end procedure
+
+   module procedure tridiag_solve
+   integer :: n
+   ! Vector dimension + memory allocation for the result.
+   n = size(b); allocate (x(n), source=0.0_wp)
+   end procedure
 end module linalg
--- test/TestLinalg.f90
+++ test/TestLinalg.f90
@@ -1,9 +1,15 @@
 module TestLinalg
+   use stdlib_math, only: all_close
+   use stdlib_linalg, only: solve
    use testdrive, only: new_unittest, unittest_type, error_type, check
+   use constants
+   use linalg
    implicit none(type, external)
    private
    public :: collect_dense_linalg_tests
    public :: collect_tridiag_linalg_tests
+
+   integer, parameter :: n = 10
 contains
 
    !-----------------------------------------------------
@@ -18,10 +24,40 @@ contains
 
    subroutine test_dense_matrix_vector_product(error)
       type(error_type), allocatable, intent(out) :: error
+      real(wp) :: A(n, n), x(n)
+      real(wp) :: y(n), z(n)
+
+      ! Random matix and vector.
+      call random_number(A); call random_number(x)
+
+      ! Reference implementation.
+      y = matmul(A, x)
+
+      ! Student implementation.
+      z = dense_matvec(A, x)
+
+      ! Check that the two match.
+      call check(error, all_close(y, z))
+      if (allocated(error)) return
    end subroutine
 
    subroutine test_gaussian_elimination(error)
       type(error_type), allocatable, intent(out) :: error
+      real(wp) :: A(n, n), b(n)
+      real(wp) :: x(n), y(n)
+
+      ! Random matix and vector.
+      call random_number(A); call random_number(b)
+
+      ! Reference implementation.
+      x = solve(A, b)
+
+      ! Student implementation.
+      y = dense_solve(A, b)
+
+      ! Check that the two match.
+      call check(error, all_close(x, y))
+      if (allocated(error)) return
    end subroutine
 
    !-----------------------------------------------------------
@@ -36,10 +72,44 @@ contains
 
    subroutine test_tridiag_matrix_vector_product(error)
       type(error_type), allocatable, intent(out) :: error
+      type(tridiagonal) :: A
+      real(wp) :: coefs(3)
+      real(wp) :: x(n), y(n), z(n)
+
+      ! Random matix and vector.
+      call random_number(coefs); call random_number(x)
+      A = tridiagonal(coefs(1), coefs(2), coefs(3), n)
+
+      ! Reference implementation.
+      y = matmul(dense(A), x)
+
+      ! Student implementation.
+      z = tridiag_matvec(A, x)
+
+      ! Check that the two match.
+      call check(error, all_close(y, z))
+      if (allocated(error)) return
    end subroutine
 
    subroutine test_tridiag_solver(error)
       type(error_type), allocatable, intent(out) :: error
+      type(tridiagonal) :: A
+      real(wp) :: coefs(3)
+      real(wp) :: x(n), y(n), b(n)
+
+      ! Random matix and vector.
+      call random_number(coefs); call random_number(b)
+      A = tridiagonal(coefs(1), coefs(2), coefs(3), n)
+
+      ! Reference implementation.
+      x = solve(dense(A), b)
+
+      ! Student implementation.
+      y = tridiag_solve(A, b)
+
+      ! Check that the two match.
+      call check(error, all_close(x, y))
+      if (allocated(error)) return
    end subroutine
 
 end module
